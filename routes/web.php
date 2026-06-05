051ea8f3e2e684f0a86062061900f8de6886ef72
Daniel McFeeters <fiforms@gmail.com>
2026-06-05 18:23:04 -0400

    Fix route ordering: register auth routes before SPA wildcard

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+require __DIR__.'/auth.php';
+
 // All non-API requests return the Vue SPA shell
 Route::get('/{any}', function () {
     return view('app');
 })->where('any', '.*');
-
-require __DIR__.'/auth.php';
